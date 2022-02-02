9bcfe5c836732fe30c0180f531b3abf71b0afc04
Bernard Thomas <bernarthomas@free.fr>
2022-02-03 00:09:28 +0100

    Ports template interface

---
+++ Port/Templating/TemplateInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Port\Templating;
+
+/**
+ * Contrat moteur de templates
+ */
+interface TemplateInterface
+{
+    /**
+     * @param string $nomFichier
+     * @param array $parametres
+     * @return string
+     */
+    public function render(string $nomFichier, array $parametres = []): string;
+}
